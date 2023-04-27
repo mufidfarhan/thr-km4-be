2eefc858240347edd2f76bdbd118fb86b244f25d
mufidfarhan <mufidfarhan68@gmail.com>
2023-04-27 18:00:15 +0700

    remove element

--- remove-element/main.go
+++ remove-element/main.go
@@ -1,9 +1,22 @@
 package main
 
-func removeLeftRight(arr []any, position string) []any {
-	//yout code here!
+import "fmt"
 
-	return nil
+func removeLeftRight(arr []any, position string) []any {
+
+	newArr := []any{}
+	if position == "left" {
+		newArr = arr[1:]
+	} else if position == "right" {
+		newArr = arr[:len(arr)-1]
+	}
+
+	return newArr
 }
 
-func main() {}
+func main() {
+	fmt.Println(removeLeftRight([]any{1, 2, 3, 4, 5}, "left"))
+	fmt.Println(removeLeftRight([]any{1, 2, 3, 4, 5}, "right"))
+	fmt.Println(removeLeftRight([]any{"Edo", "Budi", "Joko", "Tono"}, "left"))
+	fmt.Println(removeLeftRight([]any{"Edo", "Budi", "Joko", "Tono"}, "right"))
+}
--- remove-unrelated-data/main.go
+++ remove-unrelated-data/main.go
@@ -1,9 +1,21 @@
 package main
 
+import "fmt"
+
 func removeUnrelated(dataMap map[string]any, key string) map[string]any {
 	//your code here
 
-	return nil
+	newDataMap := dataMap
+	delete(newDataMap, key)
+
+	return newDataMap
 }
 
-func main() {}
+func main() {
+
+	fmt.Println(removeUnrelated(map[string]any{"name": "Edo", "age": 20, "address": "Jakarta"}, "address"))
+
+	fmt.Println(removeUnrelated(map[string]any{"run": "lari", "jump": "loncat", "swim": "berenang"}, "jump"))
+
+	fmt.Println(removeUnrelated(map[string]any{"satu": "ichi", "dua": "ni", "tiga": "san", "empat": "yon", "lima": "go"}, "tiga"))
+}
